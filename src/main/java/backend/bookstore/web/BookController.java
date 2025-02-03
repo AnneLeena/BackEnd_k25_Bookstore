c5b202d6af5548dcad1c39f09c66ec997435c593
unknown <bhd683@myy.haaga-helia.fi>
2025-02-03 22:46:45 +0200

    CRUD

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -2,8 +2,12 @@ package backend.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import backend.bookstore.domain.BookstoreRepository;
+import org.springframework.web.bind.annotation.RequestMethod;
+import backend.bookstore.domain.Book;
+
 
 @Controller
 
@@ -20,6 +24,27 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
 
         return "booklist";
-    }    
+    }
+    
+    @RequestMapping(value= "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method=RequestMethod.POST)
+    public String save (Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method=RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
+    }
+    
+    
+    
     
 }
