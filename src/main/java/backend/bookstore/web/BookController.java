3374c3887077f3bbd349d2fb50a24a1280bb298e
unknown <bhd683@myy.haaga-helia.fi>
2025-02-03 22:01:34 +0200

    Listpage added

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -1,17 +1,25 @@
 package backend.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import backend.bookstore.domain.BookstoreRepository;
 
 @Controller
 
 public class BookController {
 
-    @RequestMapping("/index")
-    @ResponseBody
-    public String returnMessage () {
-        return "Tästä se alkaa";
+    private final BookstoreRepository repository;
+
+    public BookController(BookstoreRepository repository) {
+        this.repository = repository;
     }
+
+    @RequestMapping(value= {"/", "/booklist"})
+    public String bookList (Model model) {
+        model.addAttribute("books", repository.findAll());
+
+        return "booklist";
+    }    
     
 }
